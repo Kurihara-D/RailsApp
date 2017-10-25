be21d94b89fc27799a5f8e8bbb413f9a2cc32fae
kurihara-D <na.ur666@gmail.com>
2017-10-25 15:55:13 +0900

    ユーザーと投稿の紐付け

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   before_action :authenticate_user, { only: [:index, :show, :edit, :update] }
   before_action :forbid_login_user, { only: [:new, :create, :login_form, :login] }
-  before_action :ensure_correct_user, { only: [:edit, :update] }
+  before_action :ensure_correct_user, { only: [:edit, :update, :destroy] }
   
   def index
     @users = User.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   validates :name, { presence: true }
   validates :email, { presence: true, uniqueness: true }
   validates :password, { presence: true }
-    # has_many :posts
-
+  
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
